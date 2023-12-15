3032006ceea18894364306d5fc4cedecdb8cf025
Thomas Wagener <thomas.wagener@fischantiquariat.de>
2023-12-15 13:57:40 +0100

    First commit to ms1 bubble sort

---
+++ ms1/bubble_sort/src/main.rs
@@ -0,0 +1,91 @@
+pub(crate) mod prng;
+
+//use core::num;
+use std::io;
+use std::io::Write;
+
+use prng::Prng;
+
+fn make_random_vec(num_items: i32, max: i32) -> Vec<i32> {
+    // Prepare a Prng.
+    let mut prng = Prng::new();
+
+    let mut vec: Vec<i32> = Vec::with_capacity(num_items as usize);
+    for _ in 0..num_items {
+        vec.push(prng.next_i32(0, max));
+    }
+    return vec;
+}
+
+// Print at most num_items items.
+fn print_vec(vec: &Vec<i32>, num_items: i32) {
+    let mut max = vec.len();
+    if max > num_items as usize {
+        max = num_items as usize;
+    }
+
+    let mut string = String::new();
+    string.push_str("[");
+
+    if max > 0usize {
+        string.push_str(&vec[0].to_string());
+    }
+
+    for i in 1usize..max {
+        string.push_str(" ");
+        string.push_str(&vec[i].to_string());
+    }
+    string.push_str("]");
+    println!("{string}");
+}
+// ...
+// Prompt the user for an i32.
+fn get_i32(prompt: &str) -> i32 {
+    print!("{prompt}");
+    io::stdout().flush().unwrap();
+
+    let mut str_value = String::new();
+    io::stdin()
+        .read_line(&mut str_value)
+        .expect("Error reading input");
+
+    let trimmed = str_value.trim();
+    return trimmed.parse::<i32>()
+        .expect("Error parsing integer");
+}
+
+fn bubble_sort(vec: &mut Vec<i32>) {
+    for i in 1..vec.len() {
+        for j in 0..(vec.len() - i) {
+            if vec[j] > vec[j+1] {
+                let temp = vec[j + 1];
+                vec[j + 1] = vec[j];
+                vec[j] = temp;
+            }
+        }
+    }
+}
+
+fn check_sorted(vec: &Vec<i32>) {
+    let mut sorted = true;
+    let mut i = 0;
+    while sorted && i < vec.len() - 1 {
+        sorted = vec[i] <= vec[i + 1];
+        i += 1;
+    }
+    if sorted {
+        println!("The vector is sorted!");
+    } else {
+        println!("The vector is NOT sorted!");
+    }
+}
+
+fn main() {
+    let num_items = get_i32("Please specify number of items to be sorted: ");
+    let max = get_i32("Please specify the maximum value for an item: ");
+    let mut vec = make_random_vec(num_items, max);
+    print_vec(&vec, num_items);
+    bubble_sort(&mut vec);
+    print_vec(&vec, num_items);
+    check_sorted(&vec); 
+}
---
+++ ms1/bubble_sort/src/prng.rs
@@ -0,0 +1,46 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+// ...
+
+// ************
+// *** Prng ***
+// ************
+pub struct Prng {
+    seed: u32,
+}
+
+impl Prng {
+    pub fn new() -> Self {
+        let mut prng = Self { seed: 0 };
+        prng.randomize();
+        return prng;
+    }
+
+    fn randomize(&mut self) {
+        let millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis();
+        self.seed = millis as u32;
+    }
+
+    // Return a pseudorandom value in the range [0, 2147483647].
+    pub fn next_u32(&mut self) -> u32 {
+        self.seed = self.seed.wrapping_mul(1_103_515_245).wrapping_add(12_345);
+        self.seed %= 1 << 31;
+        return self.seed;
+    }
+
+    // Return a pseudorandom value in the range [0.0, 1.0).
+    pub fn next_f64(&mut self) -> f64 {
+        let f = self.next_u32() as f64;
+        return f / (2147483647.0 + 1.0);
+    }
+
+    // Return a pseudorandom value in the range [min, max).
+    pub fn next_i32(&mut self, min: i32, max: i32) -> i32 {
+        let range = (max - min) as f64;
+        let result = min as f64 + range * self.next_f64();
+        return result as i32;
+    }
+}

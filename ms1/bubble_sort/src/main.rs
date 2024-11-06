d8814b39985772e98a57f4a992fcf573197e1821
Thomas Wagener <thomas.wagener@gls-group.com>
2024-11-06 09:10:57 +0100

    Added basic tests

--- ms1/bubble_sort/src/main.rs
+++ ms1/bubble_sort/src/main.rs
@@ -31,7 +31,9 @@ fn print_vec(vec: &[i32], num_items: i32) {
         string.push_str(&vec[0].to_string());
     }
 
-    vec.iter().for_each(|&x| string.push_str(&x.to_string()));
+    vec[0..max]
+        .iter()
+        .for_each(|&x| string.push_str(format!(" {}", &x).as_str()));
     string.push(']');
     println!("{string}");
 }
@@ -79,8 +81,63 @@ fn main() {
     let num_items = get_i32("Please specify number of items to be sorted: ");
     let max = get_i32("Please specify the maximum value for an item: ");
     let mut vec = make_random_vec(num_items, max);
-    print_vec(&vec, num_items);
+    print_vec(&vec, 10);
     bubble_sort(&mut vec);
-    print_vec(&vec, num_items);
+    print_vec(&vec, 10);
     check_sorted(&vec);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_make_random_vec() {
+        let num_items = 10;
+        let max = 100;
+        let vec = make_random_vec(num_items, max);
+        assert_eq!(vec.len(), num_items as usize);
+        for &x in vec.iter() {
+            assert!(x >= 0 && x <= max);
+        }
+    }
+
+    #[test]
+    fn test_bubble_sort() {
+        let mut vec = vec![3, 2, 1];
+        bubble_sort(&mut vec);
+        assert_eq!(vec, vec![1, 2, 3]);
+    }
+
+    #[test]
+    fn test_check_sorted() {
+        let vec = vec![1, 2, 3];
+        check_sorted(&vec);
+        let vec = vec![3, 2, 1];
+        check_sorted(&vec);
+    }
+
+    #[test]
+    fn test_check_identical() {
+        let num_items = 10;
+        let max = 100;
+        let vec = make_random_vec(num_items, max);
+        let mut vec2 = vec.clone();
+        bubble_sort(&mut vec2);
+        let (is_identical, residual) =
+            vec.iter()
+                .fold((true, vec2), |(is_identical, mut residual), &x| {
+                    if is_identical {
+                        if let Some(pos) = residual.iter().position(|&y| y == x) {
+                            residual.remove(pos);
+                            (is_identical, residual)
+                        } else {
+                            (false, residual)
+                        }
+                    } else {
+                        (is_identical, residual)
+                    }
+                });
+        assert!(is_identical && residual.is_empty());
+    }
 }
